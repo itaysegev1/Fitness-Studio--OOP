a3464ed63e9a46e4cfece3896a9a336ad6c9fcec
sasa0412 <timsitmelosa@gmail.com>
2024-12-04 17:20:20 +0200

    bl;

--- src/gym/customers/Client.java
+++ src/gym/customers/Client.java
@@ -1,8 +1,28 @@
 package gym.customers;
 
-public class Client extends Person {
+import gym.customers.Gender;
+import gym.customers.Person;
+import gym.management.Sessions.Session;
 
-    public Client(Person person) {
-        super(person);
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client extends Person {
+    protected List<Session>  l;
+
+    public Client(String name, int money, Gender gender, String birth) {
+        super(name, money, gender, birth);
+        l = new ArrayList<Session>();
+        Client c1 = new Client(name, money, gender, birth);
     }
+    public List<Session> getL() {
+        return l;
+    }
+    public void setL(List<Session> l) {
+        this.l = l;
+    }
+    public static void registertoclass(){
+
+    }
+
 }
--- src/gym/management/Sessions/Instructor.java
+++ src/gym/management/Sessions/Instructor.java
@@ -2,9 +2,27 @@ package gym.management.Sessions;
 
 import gym.customers.Gender;
 import gym.customers.Person;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Instructor extends Person {
-    public Instructor(String name, int money, Gender gender, String birth) {
+    protected double salary;
+    protected List<SessionType>l;
+
+    public Instructor(String name, int money, Gender gender, String birth,double salary) {
         super(name, money, gender, birth);
+        this.l = new ArrayList<SessionType>();
+        this.salary = salary;
+    }
+
+
+    public double getSalary() {
+        return salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    private boolean cancoach(){
+        return true;
     }
 }
